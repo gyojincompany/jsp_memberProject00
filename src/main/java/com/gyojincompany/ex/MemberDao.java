9f254540a73ad852f77d35435830e725e3b73534
JBEDU000 <JBEDU000@JBEDU00>
2022-04-25 13:10:24 +0900

    로그인 기능 추가

--- src/main/java/com/gyojincompany/ex/MemberDao.java
+++ src/main/java/com/gyojincompany/ex/MemberDao.java
@@ -94,4 +94,112 @@ public class MemberDao {
 		
 		return dbFlag;//가입이 되어 있으면 1이 반환, 아니면 0이 반환
 	}
+	
+	public int userCheck(String id, String pw) {
+		
+		int dbFlag = 0;
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;//sql 실행 객체
+		ResultSet rs = null;
+		
+		String sql = "SELECT pw from web_members where id = ?";
+		
+		try {
+			Class.forName(driverName);//jdbc 드라이버 로딩
+			conn = DriverManager.getConnection(url, user, password);//DB 연동			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {//조건이 참이면 DB에 입력된 아이디가 존재함
+				String dbPw = rs.getString("pw");
+				if(dbPw.equals(pw)) {
+					dbFlag = 1;//로그인 성공
+				} else {
+					dbFlag = 2;//비번이 틀림
+				}
+							
+			} else { //조건이 거짓이면 DB에 입력된 아이디 존재하지 않음
+				dbFlag = 0;//회원자체가 아님(db에 아이디 존재하지 않음)
+			}
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(rs != null) {
+					rs.close();
+				}
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(conn != null) {
+					conn.close();
+				}
+			} catch(Exception e) {
+				e.printStackTrace();
+			}			
+			
+		}		
+		
+		
+		return dbFlag;
+		//1이 반환되면 로그인성공, 0이 반환되면 회원아님(id 없음), 2가 반환되면 비번만 틀림
+	}
+	
+	public MemberDto getMemberInfo(String id) {
+		
+		MemberDto dto = null;
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;//sql 실행 객체
+		ResultSet rs = null;
+		
+		String sql = "SELECT * from web_members where id = ?";
+		
+		try {
+			Class.forName(driverName);//jdbc 드라이버 로딩
+			conn = DriverManager.getConnection(url, user, password);//DB 연동			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {//조건이 참이면 DB에 입력된 아이디가 존재함
+				
+				//DB에 저장되어 있던 해당 아이디의 데이터 가져오기
+				String dbId = rs.getString("id");
+				String dbPw = rs.getString("pw");
+				String dbName = rs.getString("name");
+				String dbEmail = rs.getString("email");
+				String dbAddr = rs.getString("address");
+				
+				dto.setId(dbId);
+				dto.setPw(dbPw);
+				dto.setName(dbName);
+				dto.setEmail(dbEmail);
+				dto.setAddress(dbAddr);
+			}
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(rs != null) {
+					rs.close();
+				}
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(conn != null) {
+					conn.close();
+				}
+			} catch(Exception e) {
+				e.printStackTrace();
+			}			
+			
+		}	
+		
+		return dto;
+	}
 }
